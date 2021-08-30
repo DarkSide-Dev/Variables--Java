72bae9a1dfa7d47e68371765217f487d553f220a
Joao Oliveira <vitinho.oliv@gmail.com>
2021-08-29 23:23:06 -0300

    Commit

---
+++ Variables/src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int age = 18; //Initialization
+		boolean isDeveloper = true;
+		char firstLetter = 'J';
+		String country = "Brazil";
+		
+		System.out.println("Hello World");
+		System.out.println("Guess Who's Back");
+		System.out.println("It's me, DarkSide Dev");		
+		
+		System.out.println("I'm "+age+" years old");
+		System.out.println("Am I a developer? "+isDeveloper);
+		System.out.println("The first letter of my real name is "+firstLetter);
+		System.out.println("I'm from " + country);
+		
+	}
+
+}
